082e60b36425ea8aeeb9b7c94caa9790a46da698
patricksardinha <patricksardinha.pro@gmail.com>
2026-09-25 16:18:23 +0200

    Update explore_pdf.py

--- explore_pdf.py
+++ explore_pdf.py
@@ -8,6 +8,9 @@ def separator(page_number: int) -> str:
     return f"\n\n========== PAGE {page_number} ==========\n\n"
 
 
+def separator_table() -> str:
+    return f"\n\n========== TABLE ==========\n\n"
+
 # --- pypdf ---
 reader = PdfReader(path)
 with open("data/extrait_pypdf.txt", "w", encoding="utf-8") as f:
@@ -27,14 +30,17 @@ with open("data/extrait_pymupdf.txt", "w", encoding="utf-8") as f:
 
 with open("data/extrait_table_pymupdf.txt", "w", encoding="utf-8") as f:
     for page in doc:
-        tables = page.find_tables()
-        for table in tables:
-            f.write("------------ Normal ---------------")
-            f.write(table.extract())
-            f.write("------------ Pandas ---------------")
-            f.write(table.to_pandas())
-            f.write("------------ Markdown ---------------")
-            f.write(table.to_markdown())
+        f.write(separator(page.number + 1))
+        tabs = page.find_tables()
+        print(f"{len(tabs.tables)} tableau(x) trouvé(s) sur la page {page.number + 1}.")
+
+        for tab in tabs:
+            f.write(separator_table())
+            data = tab.extract()
+            print(f"len(data): {len(data)} | len(data[0]): {len(data[0])}")
+            for line in data:
+                l = " | ".join(cell if cell is not None else "" for cell in line)
+                f.write(f"{l}\n")
 
 print(f"pymupdf : {doc.page_count} pages -> data/extrait_pymupdf.txt")
 doc.close()
